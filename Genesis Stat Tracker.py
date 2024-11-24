ce23cf6c1f7f70bf2520b95cc0f7f65535a1c31c
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-11-24 16:13:32 +1100

    Clean up code

--- Genesis Stat Tracker.py
+++ Genesis Stat Tracker.py
@@ -3,7 +3,6 @@ import sys
 from data import config
 sys.dont_write_bytecode = True
 sys.path.append(pathlib.Path(__file__).parent.resolve())
-from ahk import AHK
 def main():
     from data import main_loop
     main_loop.start()
